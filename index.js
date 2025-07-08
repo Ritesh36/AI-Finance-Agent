73a14eba2e6b0fa20951d910ab276c9e6b3e4dd1
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-07-08 11:45:21 +0530

    declare function defination, tool call, etc

--- index.js
+++ index.js
@@ -8,18 +8,63 @@ export async function main() {
     const completion = await groq.chat.completions.create({
       messages: [
         {
+          role: "system",
+          content: 
+          `Your name is Jarvis, a helpful AI financial assistant. 
+          You keep track of user expenses and provide insights on spending habits. 
+          Current datetime: ${new Date().toUTCString()}.`,
+        },
+        {
           role: "user",
-          content: "",
+          content: "Hi",
         },
       ],
-      // Change model to compound-beta to use agentic tooling
-      // model: "llama-3.3-70b-versatile",
       model: "llama-3.3-70b-versatile",
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "getTotalExpenses",
+            description: "Get the total expenses for the month",
+            parameters: {
+              type: "object",
+              properties: {
+                from : {
+                  type: "string",
+                  description: "The start date for the expense report (YYYY-MM-DD)",
+                },
+                to : {
+                  type: "string",
+                  description: "The end date for the expense report (YYYY-MM-DD)",
+                },
+              },
+            }
+          }
+        }
+      ],
     });
 
-    console.log(completion.choices[0]?.message?.content || "");
-    // Print all tool calls
-    // console.log(completion.choices[0]?.message?.executed_tools || "");
+    console.log(JSON.stringify(completion.choices[0], null, 2));
+
+    const toolCalls = completion.choices[0].message.tool_calls;
+    if(!toolCalls) {
+      console.log(`Assitant : ${completion.choices[0].message.content}`);
+      return;
+    }
+
+    for(const tool of toolCalls){
+      const functionName = tool.function.name;
+      const functionArgs = JSON.parse(tool.function.arguments);
+
+      if(functionName === "getTotalExpenses") {
+        const expenses = getTotalExpenses(functionArgs);
+      }
+    }
 }
 
 main();
+
+function getTotalExpenses({ from, to }) {
+  console.log("Calculating total expenses...");
+  return "500INR"; 
+}
